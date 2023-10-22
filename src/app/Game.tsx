8e7ef62b2cebc97316ba994b476bbcd8d4869bfb
aelynna <alina.klapproth@gmail.com>
2023-10-22 19:39:22 +0200

    created dice

--- src/app/Game.tsx
+++ src/app/Game.tsx
@@ -1,8 +1,39 @@
+import { useState } from "react";
+
 function Game() {
   return (
-    <>
-    </>
-  )
+    <div className="flex space-x-4 bg-white p-8">
+      <RandomDice />
+      <RandomDice />
+    </div>
+  );
 }
 
-export default Game
+function Dice(props: { number: number; onClick: () => void }) {
+  return (
+    <button
+      onClick={props.onClick}
+      className="flex h-12 w-12 items-center justify-center rounded-md bg-black text-white hover:bg-gray-800"
+    >
+      <div>{props.number}</div>
+    </button>
+  );
+}
+
+function RandomDice() {
+  function handleClick() {
+    setNumber(randomIntFromInterval(1, 6));
+  }
+  function randomIntFromInterval(min: number, max: number) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+  const [number, setNumber] = useState(1);
+
+  return (
+    <>
+      <Dice onClick={handleClick} number={number} />
+    </>
+  );
+}
+
+export default Game;
